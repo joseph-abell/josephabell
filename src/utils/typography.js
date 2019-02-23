866540531621960149b11bde03e7013c063729e4
Joseph Abell <joseph.abell@piksel.com>
2019-02-23 23:27:51 +0000

    update colours so h2 works

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -3,6 +3,15 @@ import Theme from 'typography-theme-twin-peaks';
 
 Theme.overrideThemeStyles = () => {
   return {
+    h1: {
+      color: `rgba(107,187,233,1)`,
+    },
+    h2: {
+      color: `rgba(107,187,233,1)`,
+    },
+    h3: {
+      color: `rgba(107,187,233,1)`,
+    },
     a: {
       color: `rgba(107,187,233,0.8)`,
       backgroundImage: `none`,
