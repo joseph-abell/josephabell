f102095ff385c912c9e2a10a98fc6709d2052473
Joseph Abell <jsphabll@gmail.com>
2019-08-27 14:06:58 +0100

    remove twitter from bio as I've deactivated twitter

--- src/components/bio.js
+++ src/components/bio.js
@@ -31,13 +31,8 @@ function Bio() {
               }}
             />
             <p>
-              <strong>{author}</strong> is a Ukulele playing Senior Javascript
-              Developer from York, UK. He blogs to help remind him about things
-              he has learned.
-            <br />
-              <a href={`https://twitter.com/${social.twitter}`}>
-                Follow him on Twitter
-              </a>
+              <strong>{author}</strong> is a Ukulele playing Wevb Dev from York, UK.
+              He blogs to help remind him about things he has learned.
             </p>
           </div>
         );
